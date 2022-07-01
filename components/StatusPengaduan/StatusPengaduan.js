7066fd5b1eef954e05d2c399551e6a916eab987b
ZhafranT <zhaftosa@gmail.com>
2022-07-01 13:25:16 +0700

    update style

--- components/StatusPengaduan/StatusPengaduan.js
+++ components/StatusPengaduan/StatusPengaduan.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { SIZES, FONTS, COLORS } from '../../constants';
 
 const StatusPengaduan = ({ item, lookdetail }) => {
   return (
-    <TouchableOpacity style={{ margin: 8, marginRight: 12, marginLeft: 12 }} onPress={() => lookdetail(item)}>
+    <TouchableOpacity style={{ justifyContent: 'center', alignItems: 'center', marginVertical: 10 }} onPress={() => lookdetail(item)}>
       <View
         style={{
-          backgroundColor: '#17C3B2',
+          width: 355,
+          backgroundColor: COLORS.primary3,
+          paddingHorizontal: 8,
+          marginHorizontal: 10,
           padding: 8,
           borderRadius: 8,
         }}>
@@ -17,10 +21,12 @@ const StatusPengaduan = ({ item, lookdetail }) => {
             alignItems: 'center',
           }}>
           <View>
-            <Text>{item.nama}</Text>
-            <Text>{item.jenisPengaduan}</Text>
+            <Text style={styles.textStatus}>{item.nama}</Text>
+            <Text style={styles.textStatus}>{item.jenisPengaduan}</Text>
           </View>
-          <Text>{item.respon_pengaduan.statusPengaduan}</Text>
+          <View style={styles.Status}>
+            <Text style={[styles.textStatus]}>{item.respon_pengaduan.statusPengaduan}</Text>
+          </View>
         </View>
       </View>
     </TouchableOpacity>
@@ -29,4 +35,16 @@ const StatusPengaduan = ({ item, lookdetail }) => {
 
 export default StatusPengaduan;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  textStatus: {
+    fontSize: SIZES.medium,
+    fontFamily: FONTS.semiBold,
+  },
+  Status: {
+    backgroundColor: COLORS.primary2,
+    width: 115,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 6,
+  },
+});
